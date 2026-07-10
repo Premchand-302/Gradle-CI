04c52fe33b079269bac58927b468695fb78af892
Premchand-302 <307473@ust.com>
2026-07-10 09:11:42 +0530

    commit

--- Gradle_Project/src/test/java/com/ust/sdet/tests/OrderTest.java
+++ Gradle_Project/src/test/java/com/ust/sdet/tests/OrderTest.java
@@ -2,6 +2,7 @@ package com.ust.sdet.tests;
 
 import com.ust.sdet.builder.OrderBuilder;
 import com.ust.sdet.factory.OrderFactory;
+import com.ust.sdet.model.Order;
 import com.ust.sdet.repository.OrderRepository;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeAll;
@@ -37,10 +38,9 @@ public class OrderTest {
     @Test
     void createsOrder() {
 
-        factory.persisted(
-                OrderBuilder.anOrder()
-                        .withQty(3)
-                        .build()
+        factory.persisted(OrderBuilder.anOrder()
+                .withQty(3)
+                .build()
         );
 
         assertEquals(1, repo.count());
@@ -49,11 +49,48 @@ public class OrderTest {
     @Test
     void countsOrders() {
 
+        factory.persisted(OrderBuilder.anOrder()
+                .build()
+        );
+
+        factory.persisted(OrderBuilder.anOrder()
+                .build()
+        );
+
+        assertEquals(2, repo.count());
+    }
+
+    @Test
+    void findsOrderBySku() {
+
         factory.persisted(
                 OrderBuilder.anOrder()
+                        .withSku("SKU-100")
                         .build()
         );
 
-        assertEquals(1, repo.count());
+        Order order = repo.findBySku("SKU-100");
+
+        assertEquals("SKU-100", order.sku());
+        assertEquals(1, order.qty());
+    }
+
+    @Test
+    void countsOnlyShippedOrders() {
+
+        factory.persisted(
+                OrderBuilder.anOrder()
+                        .withShipped(true)
+                        .build()
+        );
+
+        factory.persisted(
+                OrderBuilder.anOrder()
+                        .withSku("SKU-2")
+                        .build()
+        );
+
+        assertEquals(2, repo.count());
+        assertEquals(1, repo.countShipped());
     }
 }
